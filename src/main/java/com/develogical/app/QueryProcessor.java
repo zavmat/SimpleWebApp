b0c1097c8536ca59ec12ae265ff09b05681a6df5
zavmat <zavarzinm@gmail.com>
2020-02-17 15:07:40 +0000

    Break QueryProcessor for Shakespeare

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -3,9 +3,10 @@ package com.develogical.app;
 public class QueryProcessor {
     public String process(String query) {
         if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+            /*return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
+                    "writer in the English language and the world's pre-eminent dramatist.";*/
+            return "Mistake";
         }
         if (query.toLowerCase().contains("blake")) {
             return "William Blake was an English poet, painter, and printmaker." +
